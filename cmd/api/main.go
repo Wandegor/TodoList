8350532371911256bfe47bdb67a876ade1e7c8bc
wander122156 <sov48656@mail.ru>
2026-08-15 14:57:26 +0300

    feat: сделал простой endpoint(без бд)

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,17 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-
 	var tasks = []string{"go", "python"}
 
-	printTasks(tasks)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "hello")
+		printTasks(tasks, w)
+	})
+
+	log.Printf("Server Start on port 8080")
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func printTasks(taskItems []string) {
+func printTasks(taskItems []string, w http.ResponseWriter) {
 	for index, task := range taskItems {
-		fmt.Printf("%d: %s\n", index+1, task)
+		fmt.Fprintln(w, index+1, task)
 	}
 }

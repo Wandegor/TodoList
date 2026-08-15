cc84514a31d4ab8ec777d46989efbe46be75f8b5
wander122156 <sov48656@mail.ru>
2026-08-15 15:38:13 +0300

    feat: сделал минимальные запись и получение данных из бд

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,28 +1,67 @@
 package main
 
 import (
+	"ISpringTODOList/internal/database"
+	"ISpringTODOList/internal/models"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func main() {
-	var tasks = []string{"go", "python"}
+
+	log.Printf("Server Start on port 8080")
+	db, err := database.Connect()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Database Connect Success")
+
+	err = db.AutoMigrate(&models.Task{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Database AutoMigrate Success")
+
+	tasks := []models.Task{
+		{
+			Text: "first task",
+		},
+		{
+			Text: "another",
+		},
+	}
+
+	for _, task := range tasks {
+		if err := db.Create(&task).Error; err != nil {
+			log.Fatal(err)
+		}
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "hello")
+	})
+
+	http.HandleFunc("/tasks", func(w http.ResponseWriter, r *http.Request) {
+		var tasks []models.Task
+
+		if err := db.Find(&tasks).Error; err != nil {
+			log.Fatal(err)
+		}
+
 		printTasks(tasks, w)
 	})
 
-	log.Printf("Server Start on port 8080")
-	err := http.ListenAndServe(":8080", nil)
-
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func printTasks(taskItems []string, w http.ResponseWriter) {
+func printTasks(taskItems []models.Task, w http.ResponseWriter) {
 	for index, task := range taskItems {
 		fmt.Fprintln(w, index+1, task)
 	}
